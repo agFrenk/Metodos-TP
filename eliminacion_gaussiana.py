b18e9f6fe083a24f0b6f9166ff16673da3d1df7b
sadskinhead <afus.frenkel@gmail.com>
2024-04-12 21:49:58 -0300

    error en eliminacion eliminacion_gaussiana
    es decir cuando un elemento de la diagonal es 0

--- eliminacion_gaussiana.py
+++ eliminacion_gaussiana.py
@@ -16,8 +16,7 @@ def elim_gauss_sin_pivot(M):
       else:
         for jerror in range(i+1, len(M)):
           if M[jerror][i] != 0:
-            print("Error")
-            return 0  ## levantar error - raise   
+            raise Exception("No se puede realizar la eliminacion gaussiana con i:" + str(i) + " y j: " + str(j))
    return M
 
 def solve_sys_EGsin(M):
@@ -39,17 +38,16 @@ b_matrix =         [[ 1,  1,  1,  1, 10],
 #Punto 2
 
 
-a_matrix2 =         [[ 2,  1,  -1,  3, 13],
-                    [ -2,  0,  0,  0, -2],
-                    [ 4,  1,  -2,  4, 24],
-                    [ -6,  -1,  2,  -3, -10]]
+a_matrix2 = np.array([[ 1,  1,  0,  0, 0],
+                     [ 2,  0,  0,  0, 0],
+                     [ 0,  2,  1,  4, 0],
+                     [ 0,  0,  2,  1, 0]])
 
-print(len(a_matrix[0]))
 
 ##item a
-def elim_gauss_con_pivot(M): 
+def elim_gauss_con_pivot(M, threshold): 
    for i in range(0, len(M[0]) - 2):
-    print("i: " + str(i))  
+    # print("i: " + str(i))  
     #Buscamos la fila de maximo valor para permutar
     j_max=i
     val_j_max=0
@@ -57,32 +55,33 @@ def elim_gauss_con_pivot(M):
       if abs(M[j_pivot][i]) > val_j_max:
         val_j_max = abs(M[j_pivot][i])
         j_max = j_pivot
-    print(j_max)
-    print("M before:")
-    print(M)
+    # print(j_max)
+    # print("M before:")
+    # print(M)
     #Permutamos 
     for p in range(i, len(M[0])):
       m_ip = M[i][p]
       M[i][p] = M[j_max][p]
       M[j_max][p] = m_ip 
 
-    print("M after:")
-    print(M)
+    # print("M after:")
+    # print(M)
     #fila_pivot = M[j_max]
     #M[j_max] = M[i]
     #M[i] = fila_pivot
     #Hacemos el paso de eliminacion gaussiana 
     for j in range(i+1, len(M)):
-      if M[i][i] != 0:
+      if M[i][i] != 0:  
+        if(float(M[i][i]) <=threshold):
+          print("Advertencia de error numerico, division por valor cercano a cero")
         m_ji = float(M[j][i])/float(M[i][i])
-        print("m_ji: " + str(m_ji) + " m_ji " + str(M[j][i]) + " m_ii: " + str(M[i][i]))
+        # print("m_ji: " + str(m_ji) + " m_ji " + str(M[j][i]) + " m_ii: " + str(M[i][i]))
         for k in range(i,len(M[0])):
           M[j][k] = M[j][k] - m_ji * M[i][k]
       else:
         for jerror in range(i+1, len(M)):
           if M[jerror][i] != 0:
-            print("Error")
-            return 0  # levantar error    
+            raise Exception("No se puede realizar la eliminacion gaussiana con i:" + str(i) + " y j: " + str(j))
    return M
 
 def solve_sys_EG(M):
