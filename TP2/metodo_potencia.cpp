3700bc701e589c4eb921abff1c90b349c46f53b4
Agustin Frenkel <afus.frenkel@gmail.com>
2024-05-24 17:53:16 -0300

    metodo de la potencia en cpp funciona y se implemento correctamente knn

--- TP2/metodo_potencia.cpp
+++ TP2/metodo_potencia.cpp
@@ -8,43 +8,47 @@ using Eigen::VectorXd;
 
 #define forn(i, n) for(int i=0; i<n; i++)
 
-
-
 pair<double,VectorXd> power_iteration(const MatrixXd& A, int niter = 10000, double eps= 1e-6) {
     const int m = A.rows();
     VectorXd v(m);
     forn(i,m){
-        v[i]=1;
+        v[i]=1.0;
     }
-    double l;
+    double l = 0;
     forn(i, niter){
         v = A*v;
         v.normalize();
-        l = (double)(v.transpose() * A * v ) / (double)(v.transpose()*v);
-        if((A*v-l*v).norm()<= tol){
+        forn(i,m){
+            cout << v[i] << " ";
+        }
+        cout << '\n';
+
+        l = (v.transpose() * A * v ).value() / (v.transpose() * v).value();
+        if((A*v-l*v).norm()<= eps){
             double error = (A*v-l*v).squaredNorm();
             break;
         }
+        cout << "l:" << " "<< l << '\n';
     }
-    l = (double)(v.transpose() * A * v )/ (double)(v.transpose()*v);
+    l = (v.transpose() * A * v ).value()/ (v.transpose()*v).value();
     return {l,v};
 }
 
-pair<VectorXd, MatrixXd> eigen(MatrixXd A, int num=2, int niter=1000, double eps=1e-6){
+pair<vector<double>, vector<VectorXd>> power_iteration_deflation(MatrixXd A, int num=2, int niter=1000, double eps=1e-6){
     const int m = A.rows();
     const int n = A.cols();
-    VectorXd eigenvalues;
-    MatrixXd eigenvectors(m,n);
+    vector<double> eigenvalues;
+    vector<VectorXd> eigenvectors(m);
     VectorXd v(m);
     double l = 0.0;
     forn(i,m){
-        v[i]=1;
+        v[i]=1.0;
     }
     forn(i,n){
         MatrixXd B = A - l * (v * v.transpose());
         pair<double, VectorXd> res = power_iteration(B);
         eigenvalues.push_back(res.first);
-        eigenvectors[i]=(res.second);
+        eigenvectors[i]=res.second;
         l = res.first;
     }
     return {eigenvalues, eigenvectors};
@@ -63,14 +67,15 @@ int main() {
     v.normalize();
 
     MatrixXd Id(5,5);
-    D << 1.0, 0.0, 0.0, 0.0, 0.0,
+    Id << 1.0, 0.0, 0.0, 0.0, 0.0,
         0.0, 1.0, 0.0, 0.0, 0.0,
         0.0, 0.0, 1.0, 0.0, 0.0,
         0.0, 0.0, 0.0, 1.0, 0.0,
         0.0, 0.0, 0.0, 0.0, 1.0;
-    MatrixXd H = Id - 2*v*v.transpose(); 
+    MatrixXd H = Id - 2*(v*v.transpose()); 
+    cout << H << '\n';
     MatrixXd M = H.transpose() * D * H;
-
+    cout << M << '\n';
     pair<double, VectorXd> res = power_iteration(M);
     cout << "lambda: " << res.first << '\n';
     forn(i,5){
