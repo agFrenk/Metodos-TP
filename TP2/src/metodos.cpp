8b5154d7c60a8b3c2f373d279b778bea66cf4f3d
Agustin Frenkel <afus.frenkel@gmail.com>
2024-05-26 23:48:01 -0300

    integracion c++ y python, experimetnacion y verificacion del metodo de la potencia

---
+++ TP2/src/metodos.cpp
@@ -0,0 +1,24 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/stl.h> 
+#include "test_integration.h"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(metnum, m) {
+    m.def(
+        "power_iteration", &power_iteration,
+        "Function that calculates eigenvector",
+        py::arg("X"),
+        py::arg("niter") = 5000,
+        py::arg("epsilon") = 1e-16
+    );
+    m.def(
+        "power_iteration_deflation", &power_iteration_deflation,
+        "Function that calculates multiple eigenvectors using deflation",
+        py::arg("X"),
+        py::arg("num") = 2,
+        py::arg("niter") = 1000,
+        py::arg("epsilon") = 1e-6
+    );
+}
--- TP2/test_integration.py
+++ TP2/test_integration.py
@@ -1,57 +1,15 @@
-import ctypes
+import sys
+sys.path.append('src')
+
+import metnum
 import numpy as np
-import os
+import subprocess
 
-# Borrar el archivo .so si existe
-if os.path.exists("test_integration.so"):
-    os.remove("test_integration.so")
+# subprocess.run(['make', 'clean'], check=True)
 
-# Compilar el archivo cpp
-os.system("g++ -shared -fPIC -o test_integration.so test_integration.cpp")
+# # Ejecutar `make`
+# subprocess.run(['make'], check=True)
 
-# Listar los archivos en el directorio actual
-# os.system("ls")
-
-
-class VectorXd(ctypes.Structure):
-    _fields_ = [("data", ctypes.POINTER(ctypes.c_double)),
-                ("size", ctypes.c_int)]
-
-class Pair(ctypes.Structure):
-    _fields_ = [("first", ctypes.c_double),
-                ("second", VectorXd)]
-
-class OuterPair(ctypes.Structure):
-    _fields_ = [("first", Pair),
-                ("second", ctypes.c_double)]
-
-
-class sharedlib():
-    dlclose = ctypes.CDLL(None).dlclose  # This WON'T work on Win
-    dlclose.argtypes = (ctypes.c_void_p,)
-
-    def __init__(self, path, method, *args):
-        self.lib = ctypes.cdll.LoadLibrary(f'./{path}')
-
-        # Se explicitan los tipos de los argumentos para el método deseado
-        self.method_object = getattr(self.lib, method)
-        self.method_object.argtypes = args
-
-    def __call__(self, *args):
-        return self.method_object(*args)
-
-    def unload(self):
-        while self.dlclose(self.lib._handle)!=-1:
-            pass
-
-lib = sharedlib('test_integration.so', 'power_iteration',
-                                                            ctypes.POINTER(ctypes.c_double), 
-                                                            ctypes.c_int, 
-                                                            ctypes.c_double   
-                                                            )
-
-
-#falta crear los datos
 
 N = 10
 D = np.diag(range(N, 0, -1))
@@ -63,13 +21,5 @@ v = v / np.linalg.norm(v)
 B = np.eye(D.shape[0]) - 2 * (v @ v.T)
 
 M = B.T @ D @ B
-niter = 100
-epsilon = ctypes.c_double(1e-6)
-# Llamamos a nuestra función en C++ pasando los argumentos de entrada
-res = lib(
-    M.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-    ctypes.c_int(niter),
-    epsilon
-)
 
-lib.unload() # para poder recompilar la lib hay que cerrarla
+print(metnum.power_iteration_deflation(M, 10,niter=5000, epsilon=1e-16))
